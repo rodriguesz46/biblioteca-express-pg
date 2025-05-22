1a22d2aff4227b076f3b0b6169a1671909aead13
rodriguesz46 <rianramos155@gmail.com>
2025-05-22 14:09:58 +0000

    Adiciona script start para rodar o servidor

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('API de Biblioteca com PostgreSQL e Express!');
+});
+
+app.get('/categorias', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM categoria');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar categorias');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
